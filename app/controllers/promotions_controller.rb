6c51070dc0d6e9055bf9ca315af20110936d3ed6
DanRPS <pro.danilorps@gmail.com>
2021-03-17 10:52:04 -0300

    falta apenas 1 test

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -5,6 +5,21 @@ class PromotionsController < ApplicationController
   end
 
   def show
+    id = params[:id]
+    @promotion = Promotion.find(id)
   end
+  
+  #private
+
+#  def promotion_params
+    #params.require(:promotion).permit(
+      #:name,
+      #:description:,
+      #:code,
+      #:discount_rate,
+      #:coupon_quantity,
+      #:expiration_date
+    #)
+  #end
 
 end
