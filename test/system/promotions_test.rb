ccdadcb00a512c7d212e7e812b3e8d6ea8d06faf
DanRPS <pro.danilorps@gmail.com>
2021-03-18 12:54:13 -0300

    passa nos testes: create and attributes cannot be blank, e: create and code must be unique

--- test/system/promotions_test.rb
+++ test/system/promotions_test.rb
@@ -119,5 +119,38 @@ class PromotionsTest < ApplicationSystemTestCase
       refute promotion.valid?
       assert_includes promotion.errors[:code], 'deve ser único'
   end
-  
+
+  test 'create and attributes cannot be blank' do
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Registrar uma promoção'
+    fill_in 'Nome', with: ''
+    fill_in 'Descrição', with: ''
+    fill_in 'Código', with: ''
+    fill_in 'Desconto', with: ''
+    fill_in 'Quantidade de cupons', with: ''
+    fill_in 'Data de término', with: ''
+    click_on 'Criar promoção'
+
+    assert_text 'não pode ficar em branco', count: 5
+  end
+
+  test 'create and code must be unique' do
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Registrar uma promoção'
+    fill_in 'Código', with: 'NATAL10'
+    click_on 'Criar promoção'
+
+    assert_text 'deve ser único'
+  end
+
 end
