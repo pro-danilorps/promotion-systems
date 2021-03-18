06459ba438c5f95e3ebe973141c36f2c0aa9335b
DanRPS <pro.danilorps@gmail.com>
2021-03-18 16:04:06 -0300

    code e name precisam ser unicos

--- test/system/promotions_test.rb
+++ test/system/promotions_test.rb
@@ -97,25 +97,21 @@ class PromotionsTest < ApplicationSystemTestCase
   end
 
   test 'create and attributes cannot be blank' do
-    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
-                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
-                      expiration_date: '22/12/2033')
-
     visit root_path
     click_on 'Promoções'
     click_on 'Registrar uma promoção'
-    fill_in 'Nome', with: ''
-    fill_in 'Descrição', with: ''
-    fill_in 'Código', with: ''
-    fill_in 'Desconto', with: ''
-    fill_in 'Quantidade de cupons', with: ''
-    fill_in 'Data de término', with: ''
+    #fill_in 'Nome', with: ''
+    #fill_in 'Descrição', with: ''
+    #fill_in 'Código', with: ''
+    #fill_in 'Desconto', with: ''
+    #fill_in 'Quantidade de cupons', with: ''
+    #fill_in 'Data de término', with: ''
     click_on 'Criar promoção'
 
     assert_text 'não pode ficar em branco', count: 5
   end
 
-  test 'create and code must be unique' do
+  test 'create and code/name must be unique' do
     Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
                       code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
                       expiration_date: '22/12/2033')
@@ -123,10 +119,11 @@ class PromotionsTest < ApplicationSystemTestCase
     visit root_path
     click_on 'Promoções'
     click_on 'Registrar uma promoção'
+    fill_in 'Nome', with: 'Natal'
     fill_in 'Código', with: 'NATAL10'
     click_on 'Criar promoção'
 
-    assert_text 'deve ser único'
+    assert_text 'deve ser único', count: 2
   end
 
 end
