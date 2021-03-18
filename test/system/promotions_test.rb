b5ae1926c088d0665dafa38d4d89bbbb8d2b950e
DanRPS <pro.danilorps@gmail.com>
2021-03-18 15:18:47 -0300

    testes unitarios foram levados para o test dentro do models

--- test/system/promotions_test.rb
+++ test/system/promotions_test.rb
@@ -96,30 +96,6 @@ class PromotionsTest < ApplicationSystemTestCase
     assert_link 'Voltar'
   end
 
-  test 'attributes cannot be blank' do
-    promotion = Promotion.new
-
-    refute promotion.valid?
-    assert_includes promotion.errors[:name], 'não pode ficar em branco'
-    assert_includes promotion.errors[:code], 'não pode ficar em branco'
-    assert_includes promotion.errors[:discount_rate], 'não pode ficar em '\
-                                                      'branco'
-    assert_includes promotion.errors[:coupon_quantity], 'não pode ficar em'\
-                                                        ' branco'
-    assert_includes promotion.errors[:expiration_date], 'não pode ficar em'\
-                                                        ' branco'
-  end
-
-  test 'code must be uniq' do
-      Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
-                        code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
-                        expiration_date: '22/12/2033')
-      promotion = Promotion.new(code: 'NATAL10')
-
-      refute promotion.valid?
-      assert_includes promotion.errors[:code], 'deve ser único'
-  end
-
   test 'create and attributes cannot be blank' do
     Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
                       code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
