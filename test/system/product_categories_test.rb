d3750be2bd8e4b297ac643433793acde56431884
DanRPS <pro.danilorps@gmail.com>
2021-04-02 10:02:07 -0300

    categorias de produto passando allgreen

--- test/system/product_categories_test.rb
+++ test/system/product_categories_test.rb
@@ -2,11 +2,14 @@ require 'application_system_test_case'
 
 class ProductCategoriesTest < ApplicationSystemTestCase
 
-  test 'create product categories and show them' do
-    ProductCategory.create!(name: 'Produto Anti-Fraude', code: 'ANTIFRA')
+  def setup
+    @user = create_user
+    @product_category = ProductCategory.create!(name: 'Produto Anti-Fraude', code: 'ANTIFRA')
     ProductCategory.create!(name: 'Produto Pro-Fraude', code: 'PROFRA')
+    login_as @user
+  end
 
-    login_user
+  test 'create product categories and show them' do
     visit root_path
     click_on 'Categorias de Produto'
 
@@ -17,10 +20,7 @@ class ProductCategoriesTest < ApplicationSystemTestCase
   end
 
   test 'edit and leave blank fields to return errors' do
-    product_category = ProductCategory.create!(name: 'Produto Anti-Fraude', code: 'ANTIFRA')
-
-    login_user
-    visit edit_product_category_path(product_category)
+    visit edit_product_category_path(@product_category)
     fill_in 'Nome', with: ''
     fill_in 'Código', with: ''
     click_on 'Confirmar Alterações'
@@ -29,29 +29,22 @@ class ProductCategoriesTest < ApplicationSystemTestCase
   end
 
   test 'edit and its a success' do
-    product_category = ProductCategory.create!(name: 'Produto Anti-Fraude', code: 'ANTIFRA')
-
-    login_user
-    visit edit_product_category_path(product_category)
-    fill_in 'Nome', with: 'Produto Pro-Fraude'
-    fill_in 'Código', with: 'PROFRA'
+    visit edit_product_category_path(@product_category)
+    fill_in 'Nome', with: 'Produto Neutro-Fraude'
+    fill_in 'Código', with: 'NEUFRA'
     click_on 'Confirmar Alterações'
 
     assert_text 'Alterações feitas com sucesso!'
   end
 
   test 'destroy product category' do
-    ProductCategory.create!(name: 'Produto Anti-Fraude', code: 'ANTIFRA')
-    
-    login_user
     visit root_path
     click_on 'Categorias de Produto'
     click_on 'Produto Anti-Fraude'
+    
     accept_confirm do
       click_on 'Apagar Categoria de Produto'
     end
-
     assert_text 'Categoria apagada com sucesso!'
   end
-
 end
