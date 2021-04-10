abcee62167b921f4e5283056153c3e63d6054327
DanRPS <pro.danilorps@gmail.com>
2021-04-10 12:32:00 -0300

    testes de request allgreen | WIP Testes

--- spec/requests/promotion_api_spec.rb
+++ spec/requests/promotion_api_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe 'Promotion API Tests' do
   
   it 'show coupon' do
     headers = { "ACCEPT" => "application/json" }
-    coupon = Fabricate(:coupon)    
-    byebug
-    get "/api/v1/coupons/#{coupon.code}"
+    promotion = Fabricate(:promotion)
+    coupon = Coupon.create!(code: 'NATAL1', promotion: promotion)    
+    
+    get "/api/v1/coupons/#{coupon.code}", as: :json
 
-    expect(response).to have_http_status(:created)
+    expect(response).to have_http_status(:success)
     body = JSON.parse(response.body, symbolize_names: true)
     expect(body[:discount_rate]).to eq(coupon.discount_rate.to_s)
   end
@@ -16,7 +17,6 @@ RSpec.describe 'Promotion API Tests' do
   it 'show coupon not found ' do
     get '/api/v1/coupons/0', as: :json
 
-    assert_response :not_found, as: :json
+    expect(response).to have_http_status(:not_found)
   end
-
 end
--- spec/requests/promotion_flow_spec.rb
+++ spec/requests/promotion_flow_spec.rb
@@ -1,68 +1,63 @@
-require 'test_helper'
+require 'rails_helper'
 
-class Promotion::FlowTest < ActionDispatch::IntegrationTest
-  def setup
-    @user = create_user
-    Promotion.create!(
-      name: 'Natal',
-      description: 'Promoção de natal',
-      code: 'NATAL10',
-      discount_rate: 15,
-      coupon_quantity: 5,
-      expiration_date: '22/12/2033',
-      user: @user
-    )
-    @promotion = Promotion.last
+RSpec.describe 'Promotion Flow it' do
+  before do
+    @user = Fabricate(:user)
   end
 
-  test 'can create a promotion' do
-    login_as @user
+  it 'can create a promotion' do
+    login_as(@user)
     post '/promotions', params: {
       promotion: { name: 'Cyber Monday', description: 'Promoção de Cyber Monday',
                    code: 'CYBER15', discount_rate: 15, coupon_quantity: 5,
                    expiration_date: '22/12/2033', user: @user }
     }
 
-    assert_redirected_to promotion_path(Promotion.last)
+    expect(response).to redirect_to(promotion_path(Promotion.last))
     follow_redirect!
     assert_select 'h3', 'Cyber Monday'
   end
 
-  test 'cannot create a promotion without login' do
+  it 'cannot create a promotion without login' do
     post '/promotions', params: {
       promotion: { name: 'Cyber Monday', description: 'Promoção de Cyber Monday',
                    code: 'CYBER15', discount_rate: 15, coupon_quantity: 5,
                    expiration_date: '22/12/2033' }
     }
 
-    assert_redirected_to new_user_session_path
+    expect(response).to redirect_to(new_user_session_path)
   end
 
-  test 'cannot generate coupons without login' do
-    post generate_coupons_promotion_path(@promotion)
+  it 'cannot generate coupons without login' do
+    promotion = Fabricate(:promotion)
+    post generate_coupons_promotion_path(promotion)
 
-    assert_redirected_to new_user_session_path
+    expect(response).to redirect_to(new_user_session_path)
   end
 
-  test 'cannot update promotions without login' do
-    @promotion[:name] = 'Natal2'
+  it 'cannot update promotions without login' do
+    promotion = Fabricate(:promotion)
+    promotion[:name] = 'Natal2'
 
-    patch promotion_path(@promotion)
+    patch promotion_path(promotion)
 
-    assert_redirected_to new_user_session_path
+    expect(response).to redirect_to(new_user_session_path)
   end
 
-  test 'cannot destroy promotions without login' do
-    delete promotion_path(@promotion)
+  it 'cannot destroy promotions without login' do
+    promotion = Fabricate(:promotion)
+    delete promotion_path(promotion)
 
-    assert_redirected_to new_user_session_path
+    expect(response).to redirect_to(new_user_session_path)
   end
 
-  test 'cannot approve a promotion if owner' do
-    login_as(@user)
+  it 'cannot approve a promotion if owner' do
+    promotion = Fabricate(:promotion)
+    user = promotion.user
+    login_as(user)
 
-    post approve_promotion_path(@promotion)
+    post approve_promotion_path(promotion)
 
-    assert_equal 'Ação inválida', flash[:alert]
+    expect(flash[:alert]).to eq('Ação inválida')
   end
 end
